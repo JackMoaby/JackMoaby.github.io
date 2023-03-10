6810a73aa138f635f9d0b782678c1174ccd5063d
Prqwn <115582774+Prqwn@users.noreply.github.com>
2023-03-10 12:28:13 +0000

    Update optomised-particle-sim.js

--- scripts/optomised-particle-sim.js
+++ scripts/optomised-particle-sim.js
@@ -1,14 +1,9 @@
-if (window.innerWidth > 800){
-    var simulation_settings = [2000, 5, 0.00008, 0.0012]
-} else {
-    var simulation_settings = [1000, 2, 0.00008, 0.0012]
-}
-
-
 var particle_canvas = document.getElementById("particles-canvas");
 var particle_screen = particle_canvas.getContext("2d");
 var particles = []
 var paused = false
+var  simulation_settings
+var mouse_location = [50,50]
 
 
 particle_canvas.width = window.innerWidth; 
@@ -16,41 +11,24 @@ particle_canvas.height = window.innerHeight;
 
 
 function init(){
-    // simulation_settings = [max_particles, dot_size, delta_v, friction]
+    // FPS TEST
+    
+
+
+    // [max_particles, dot_size, delta_v, friction]
     if (window.innerWidth > 800){
-        simulation_settings = [2000, 5, 0.00008, 0.0012]
+        simulation_settings = [2500, 5, 0.00008, 0.0012]
     } else {
         simulation_settings = [1000, 2, 0.00008, 0.0012]
     }
-    
-
-    particles = []
-    mouse_location = [50, 50]
 
     for (let i = 0; i < simulation_settings[0]; i++){
         particles.push([Math.random() * 100, Math.random() * 100, 0, 0]);
     }
 }
 
-function frame(){
+function update_particles(){
     if (paused == false) {
-        // Clear the canvas
-        //particle_screen.clearRect(0, 0, particle_canvas.width, particle_canvas.height);
-
-        // Motion blur the canvas
-        particle_screen.beginPath();
-        particle_screen.fillStyle = "rgba(0, 0, 0, 0.4)";
-        particle_screen.rect(0, 0, particle_canvas.width, particle_canvas.height); 
-        particle_screen.fill();
-
-        // Draw the particles
-        for (let i = 0; i < particles.length; i++) {
-            particle_screen.beginPath();
-            particle_screen.fillStyle = "#24b5aa";
-            particle_screen.arc((particles[i][0] / 100) * particle_canvas.width , (particles[i][1] / 100) * particle_canvas.height, simulation_settings[1], 0, 2 * Math.PI)
-            particle_screen.fill();
-        }
-
         // Update the particles by their angle and velocity
         for (let i = 0; i < particles.length; i++) {
             particles[i][0] = particles[i][0] * (1 - simulation_settings[3])
@@ -72,8 +50,29 @@ function frame(){
             particles[i][0] += particles[i][2]
             particles[i][1] += particles[i][3]
         }
+        // Call the update again
+        requestAnimationFrame(update_particles);
+    }
+}
 
+function frame(){
+    if (paused == false) {
+        // Clear the canvas
+        //particle_screen.clearRect(0, 0, particle_canvas.width, particle_canvas.height);
 
+        // Motion blur the canvas
+        particle_screen.beginPath();
+        particle_screen.fillStyle = "rgba(0, 0, 0, 0.4)";
+        particle_screen.rect(0, 0, particle_canvas.width, particle_canvas.height); 
+        particle_screen.fill();
+
+        // Draw the particles
+        for (let i = 0; i < particles.length; i++) {
+            particle_screen.beginPath();
+            particle_screen.fillStyle = "#24b5aa";
+            particle_screen.arc((particles[i][0] / 100) * particle_canvas.width , (particles[i][1] / 100) * particle_canvas.height, simulation_settings[1], 0, 2 * Math.PI)
+            particle_screen.fill();
+        }
         // Call the frame again
         requestAnimationFrame(frame);
     }
@@ -115,4 +114,5 @@ onscroll = function(event){
 
 
 init();
+update_particles();
 frame();
